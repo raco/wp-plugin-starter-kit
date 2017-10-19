9d6333d492bd4053abfad3d3d9acb9c8debd9707
Victor Aigbeghian <pfmreal@gmail.com>
2017-10-19 06:30:45 +0200

    Load plugin resources from main plugin class
    Main plugin class to load  other plugin classes and functions

--- plugin-name.php
+++ plugin-name.php
@@ -19,113 +19,41 @@
  * Domain Path:       /languages
  */
 
-/**
- *-----------------------------------------
- * Do not delete this line
- * Added for security reasons: http://codex.wordpress.org/Theme_Development#Template_Files
- *-----------------------------------------
- */
-defined('ABSPATH') or die("Direct access to the script does not allowed");
-/*-----------------------------------------*/
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /*----------------------------------------------------------------------------*
  * * * ATTENTION! * * *
  * FOR DEVELOPMENT ONLY
  * SHOULD BE DISABLED ON PRODUCTION
  *----------------------------------------------------------------------------*/
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
 /*----------------------------------------------------------------------------*/
 
-/*----------------------------------------------------------------------------*
- * Plugin Settings
- *----------------------------------------------------------------------------*/
-
-/* ----- Plugin Module: Settings ----- */
-require_once plugin_dir_path(__FILE__) . 'includes/class-plugin-name-settings.php';
-
-register_activation_hook(__FILE__, array('Plugin_Name_Settings', 'activate'));
-add_action('plugins_loaded', array('Plugin_Name_Settings', 'get_instance'));
-/* ----- Module End: Settings ----- */
-
-/*----------------------------------------------------------------------------*
- * Include extensions and CPT
- *----------------------------------------------------------------------------*/
-
-/* ----- Plugin Module: CPT ----- */
-require_once plugin_dir_path(__FILE__) . 'includes/cpt/class-plugin-name-cpt.php';
-add_action('plugins_loaded', array('Plugin_Name_CPT', 'get_instance'));
-/* ----- Module End: CPT ----- */
-
-/*----------------------------------------------------------------------------*
- * Custom DB Tables
- *----------------------------------------------------------------------------*/
-
-/* ----- Plugin Module: Database ----- */
-require_once plugin_dir_path(__FILE__) . 'includes/class-plugin-name-db.php';
-
-register_activation_hook(__FILE__, array('Plugin_Name_DB', 'activate'));
-add_action('plugins_loaded', array('Plugin_Name_DB', 'db_check'));
-/* ----- Module End: Database ----- */
-
-/*----------------------------------------------------------------------------*
- * Public-Facing Functionality
- *----------------------------------------------------------------------------*/
-
-require_once plugin_dir_path(__FILE__) . 'includes/class-plugin-name.php';
-
-/*
- * Register hooks that are fired when the plugin is activated or deactivated.
- * When the plugin is deleted, the uninstall.php file is loaded.
- */
-register_activation_hook(__FILE__, array('Plugin_Name', 'activate'));
-register_deactivation_hook(__FILE__, array('Plugin_Name', 'deactivate'));
-
-add_action('plugins_loaded', array('Plugin_Name', 'get_instance'));
-
-/*----------------------------------------------------------------------------*
- * Dashboard and Administrative Functionality
- *----------------------------------------------------------------------------*/
-
-if (is_admin() && (!defined('DOING_AJAX') || !DOING_AJAX)) {
-
-    /* ----- Plugin Module: CRUD ----- */
-    require_once plugin_dir_path(__FILE__) . 'includes/admin/class-plugin-name-admin-crud-list.php';
-    /* ----- Module End: CRUD ----- */
-
-    require_once plugin_dir_path(__FILE__) . 'includes/admin/class-plugin-name-admin.php';
-    add_action('plugins_loaded', array('Plugin_Name_Admin', 'get_instance'));
-
-    require_once plugin_dir_path(__FILE__) . 'includes/admin/class-plugin-name-admin-pages.php';
-    add_action('plugins_loaded', array('Plugin_Name_Admin_Pages', 'get_instance'));
-
-    require_once plugin_dir_path(__FILE__) . 'includes/admin/class-plugin-name-admin-pages-crud.php';
-    add_action('plugins_loaded', array('Plugin_Name_Admin_Pages_CRUD', 'get_instance'));
-
-    require_once plugin_dir_path(__FILE__) . 'includes/admin/class-plugin-name-admin-pages-settings.php';
-    add_action('plugins_loaded', array('Plugin_Name_Admin_Pages_Settings', 'get_instance'));
-
+// Define PLUGIN_NAME_PLUGIN_FILE.
+if ( ! defined( 'PLUGIN_NAME_PLUGIN_FILE' ) ) {
+	define( 'PLUGIN_NAME_PLUGIN_FILE', __FILE__ );
 }
 
-/*----------------------------------------------------------------------------*
- * Register Plugin Shortcode
- *----------------------------------------------------------------------------*/
+// Include the main Plugin_Name class
+if ( ! class_exists( 'Plugin_Name' ) ) {
+	include_once dirname( __FILE__ ) . '/includes/class-plugin-name.php';
+}
 
-/* ----- Plugin Module: Shortcode ----- */
-// Admin Side
-require_once plugin_dir_path(__FILE__) . 'includes/shortcode/class-plugin-name-shortcode-admin.php';
-add_action('plugins_loaded', array('Plugin_Name_Shortcode_Admin', 'get_instance'));
+/**
+ * Main Plugin_Name instance
+ *
+ * Returns the main instance of Plugin_Name
+ * @since 	1.0.0
+ * @return	Plugin_Name
+ */
+function plugin_prefix() {
+	return Plugin_Name::get_instance();
+}
 
-// Public Side
-require_once plugin_dir_path(__FILE__) . 'includes/shortcode/class-plugin-name-shortcode-public.php';
-add_action('plugins_loaded', array('Plugin_Name_Shortcode_Public', 'get_instance'));
-/* ----- Module End: Shortcode ----- */
-
-/*----------------------------------------------------------------------------*
- * Handle AJAX Calls
- *----------------------------------------------------------------------------*/
-
-/* ----- Plugin Module: AJAX ----- */
-require_once plugin_dir_path(__FILE__) . 'includes/class-plugin-name-ajax.php';
-add_action('plugins_loaded', array('Plugin_Name_AJAX', 'get_instance'));
-/* ----- Module End: AJAX ----- */
+// Register activation and deactivation hooks
+register_activation_hook( __FILE__, array( 'Plugin_Name', 'activate' ) );
+register_deactivation_hook( __FILE__, array(  'Plugin_Name', 'deactivate' ) );
